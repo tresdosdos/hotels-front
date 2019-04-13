d9f776327181aec053e2ff346afcf421ea728886
tresdosdos <vitaliystrelok8@gmail.com>
2019-04-13 19:12:20 +0300

    add deploys

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: npm i && npm i webpack -g && npm run build && npm run start:prod
--- env.js
+++ env.js
@@ -7,7 +7,7 @@ if (process.env.NODE_ENV === 'development') {
     };
 } else {
     environment = {
-        baseUrl: '',
+        baseUrl: 'https://hotels-back.herokuapp.com',
         clientUrl: '',
     };
 }
